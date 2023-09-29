d8628c6a327b39c8e991ddcee6b69d0fab47644c
Dennis Gabriel <dennisgabr745@gmail.com>
2023-09-29 11:06:11 -0300

    Validate password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validate :password_validate
+
+  def password_validate
+    unless password =~ /^(?=.*[A-Za-z])(?=.*\d).{8,}$/
+        errors.add :password, "A senha deve conter pelo menos um número e ter no mínimo 8 caracteres"
+        # Somente para testar no console
+        puts "A senha deve conter pelo menos um número e ter no mínimo 8 caracteres"
+    end
+  end
 end
