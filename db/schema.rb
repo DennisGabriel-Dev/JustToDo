6b8bd7d2412f0129de9aba9001f49a8e8c390f1d
Dennis Gabriel <dennisgabr745@gmail.com>
2023-09-29 10:59:44 -0300

    Change sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_09_22_182131) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "task_lists", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_task_lists_on_user_id"
@@ -22,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_22_182131) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.boolean "status"
-    t.integer "task_list_id", null: false
+    t.bigint "task_list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["task_list_id"], name: "index_tasks_on_task_list_id"
